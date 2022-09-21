db65ca655abfc11dfc379b955234424be71adbb0
Ihor <dimov.igor97@gmail.com>
2022-09-21 21:11:27 +0300

    todolist10 (redux)
    todolist10 (redux)

---
+++ src/state/store.ts
@@ -0,0 +1,19 @@
+import {combineReducers, createStore} from "redux";
+import {tasksReducer} from "./tasks-reducer";
+import {todolistsReducer} from "./todolists-reducer";
+
+const rootReducer = combineReducers({
+    todolists:todolistsReducer,
+    tasks:tasksReducer
+})
+
+// type AppRootState = {
+//     todolists: TodolistType[]
+//     tasks:TasksStateType
+// }
+export type AppRootState = ReturnType<typeof rootReducer>
+
+export const store = createStore(rootReducer)
+
+// @ts-ignore
+window.store = store
